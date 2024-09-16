c5878dc0c6f62101794636e0f265f0a70da1e083
Neko <lowpolyneko@protonmail.ch>
2024-09-15 22:39:56 -0500

    fix cases

--- hw02-05.sql
+++ hw02-05.sql
@@ -2,4 +2,4 @@ SELECT Lines.Color, COUNT(StopDetails.Stop_ID)
 FROM Lines
 JOIN StopDetails ON Lines.Line_ID = StopDetails.Line_ID
 GROUP BY Lines.Line_ID
-ORDER BY COUNT(StopDetails.Stop_ID)
+ORDER BY COUNT(StopDetails.Stop_ID), Lines.Color
--- hw02-06.sql
+++ hw02-06.sql
@@ -1,5 +1,6 @@
-SELECT Stations.Station_Name, COUNT(Stops.ADA)
+SELECT Stations.Station_Name, SUM(Stops.ADA)
 FROM Stations
 INNER JOIN Stops ON Stations.Station_ID = Stops.Station_ID
+WHERE Stops.ADA > 0
 GROUP BY Stations.Station_ID
 ORDER BY COUNT(Stops.ADA) DESC, Stations.Station_Name
